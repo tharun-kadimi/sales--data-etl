33e8f0f4ab2b6641a49ce23446323c402da02291
tharun-kadimi <tharunkadimi13@gmail.com>
2024-10-31 22:27:40 +0530

    extracting of sales and customer data

--- Scripts/Extract.py
+++ Scripts/Extract.py
@@ -13,9 +13,9 @@ objectdf1= s3_client.get_object(Bucket=bucket_name,Key='sales_data.csv')
 SalesDf=pd.read_csv(objectdf1['Body'])
 print(SalesDf) 
 
-# objectdf2= s3_client.get_object(Bucket=bucket_name,Key='customer_data.json') 
-# CustomerDf=pd.read_json(objectdf2['Body'])
-# print(SalesDf) 
+objectdf2= s3_client.get_object(Bucket=bucket_name,Key='customer_data.json') 
+CustomerDf=pd.read_json(objectdf2['Body'])
+print(SalesDf) 
 
 # objectdf3= s3_client.get_object(Bucket=bucket_name,Key='product_data.xlsx') 
 # ProductDf=pd.read_excel(objectdf3['Body'])
