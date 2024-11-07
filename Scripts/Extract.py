b15ad3ef465c8d1cad12a3a865341a94de0b7d10
tharun-kadimi <tharunkadimi13@gmail.com>
2024-11-07 22:19:45 +0530

    wrote a function to return data

--- Scripts/Extract.py
+++ Scripts/Extract.py
@@ -12,21 +12,23 @@ bucket_name = 'sales--data-etl'
 sales_data='sales_data.csv'
 customer_data='customer_data.json'
 product_data='product_data.xlsx'
+def salesData():
+    objectdf1= s3_client.get_object(Bucket=bucket_name,Key=sales_data) 
+    SalesDf=pd.read_csv(objectdf1['Body'])
+    print(SalesDf) 
+    response1 = s3_client.head_object(Bucket=bucket_name, Key=sales_data)
+    last_modified_f1 = response1['LastModified']
 
-objectdf1= s3_client.get_object(Bucket=bucket_name,Key=sales_data) 
-SalesDf=pd.read_csv(objectdf1['Body'])
-print(SalesDf) 
-response1 = s3_client.head_object(Bucket=bucket_name, Key=sales_data)
-last_modified_f1 = response1['LastModified']
+    # Get today's date in UTC (same timezone as the file's LastModified)
+    today = datetime.now(timezone.utc)
 
-# Get today's date in UTC (same timezone as the file's LastModified)
-today = datetime.now(timezone.utc)
-
-# Check if the file was uploaded today
-if last_modified_f1.date() == today.date():
-    print(f"The file {sales_data} was uploaded today ({last_modified_f1.date()}).")
-else:
-    print(f"The file {sales_data} was uploaded on {last_modified_f1.date()}, not today.")
+    # Check if the file was uploaded today
+    if last_modified_f1.date() == today.date():
+        print(f"The file {sales_data} was uploaded today ({last_modified_f1.date()}).")
+        return SalesDf
+    else:
+        print(f"The file {sales_data} was uploaded on {last_modified_f1.date()}, not today.")
+        print("none")
 
 
 #customer_data_section
