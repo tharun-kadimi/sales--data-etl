55616f78733bbcb007153d068a1ef634ca47b56f
tharun-kadimi <tharunkadimi13@gmail.com>
2024-12-14 11:16:31 +0530

    made some changes for extraction code and wrote in a single function

--- Scripts/Extract.py
+++ Scripts/Extract.py
@@ -8,58 +8,28 @@ from datetime import datetime, timezone
 s3_client= boto3.client('s3')
 s3= boto3.resource('s3')
 
-bucket_name = 'sales--data-etl'
-sales_data='sales_data.csv'
-customer_data='customer_data.json'
-product_data='product_data.xlsx'
-
-def salesData(sales_data):
-    objectdf1= s3_client.get_object(Bucket=bucket_name,Key=sales_data) 
-    SalesDf=pd.read_csv(objectdf1['Body'])
-    print(SalesDf) 
-    response1 = s3_client.head_object(Bucket=bucket_name, Key=sales_data)
-    last_modified_f1 = response1['LastModified']
+def common_extraction(bucket_name,file_name):
+    file_extension=file_name.split('.')[-1]
+    if file_extension=='csv':
+        objectdf= s3_client.get_object(Bucket=bucket_name,Key=file_name) 
+        Df=pd.read_csv(objectdf['Body'])
+    elif file_extension=='json':
+        objectdf= s3_client.get_object(Bucket=bucket_name,Key=file_name) 
+        Df=pd.read_json(objectdf['Body'])
+    elif file_extension=='xlsx':
+        objectdf= s3_client.get_object(Bucket=bucket_name,Key=file_name) 
+        Df=pd.read_excel(BytesIO(objectdf['Body'].read()))
+    print(Df) 
+    response= s3_client.head_object(Bucket=bucket_name, Key=file_name)
+    last_modified= response['LastModified']
 
     # Get today's date in UTC (same timezone as the file's LastModified)
     today = datetime.now(timezone.utc)
 
     # Check if the file was uploaded today
-    if last_modified_f1.date() == today.date():
-        print(f"The file {sales_data} was uploaded today ({last_modified_f1.date()}).")
-        return SalesDf
+    if last_modified.date() == today.date():
+        print(f"The file {file_name} was uploaded today ({last_modified.date()}).")
+        return Df
     else:
-        print(f"The file {sales_data} was uploaded on {last_modified_f1.date()}, not today.")
+        print(f"The file {file_name} was uploaded on {last_modified.date()}, not today.")
         
-
-#customer_data_section
-def customerData(customer_data):
-    objectdf2= s3_client.get_object(Bucket=bucket_name,Key=customer_data) 
-    CustomerDf=pd.read_json(objectdf2['Body'])
-    print(CustomerDf) 
-    response2 = s3_client.head_object(Bucket=bucket_name, Key=customer_data)
-    last_modified_f2 = response2['LastModified']
-
-    today = datetime.now(timezone.utc)
-
-    if last_modified_f2.date() == today.date():
-        print(f"The file {customer_data} was uploaded today ({last_modified_f2.date()}).")
-        return CustomerDf
-    else:
-        print(f"The file {customer_data} was uploaded on {last_modified_f2.date()}, not today.")
-
-
-#product_data_section
-def productsData(product_data):
-    objectdf3= s3_client.get_object(Bucket=bucket_name,Key=product_data) 
-    ProductDf=pd.read_excel(BytesIO(objectdf3['Body'].read()))
-    print(ProductDf) 
-    response3 = s3_client.head_object(Bucket=bucket_name, Key=product_data)
-    last_modified_f3 = response3['LastModified']
-
-    today = datetime.now(timezone.utc)
-
-    if last_modified_f3.date() == today.date():
-        print(f"The file {product_data} was uploaded today ({last_modified_f3.date()}).")
-        return ProductDf
-    else:
-        print(f"The file {product_data} was uploaded on {last_modified_f3.date()}, not today.")
